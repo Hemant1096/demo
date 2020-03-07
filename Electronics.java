0868f4afb8a81f60433bb2a4d31fbd04d0173b43
Heman <hemant1096@outlook.com>
2020-03-07 10:40:25 +0530

    1st commit

---
+++ Electronics.java
@@ -0,0 +1,17 @@
+class Electronics{
+public static int price;
+public static class Television{
+public static void cost()                // Through Static method
+{
+System.out.println("call through static method Price is "+2000);
+}
+public  void cost1()                // Through Non-Static method
+{
+System.out.println("call through non-static method Price is "+5000);
+}  }  }
+public class Main{
+public static void main(String args[]){
+Electronics.Television.cost();                               // call  static method
+Electronics.Television tv=new Electronics.Television();      // call non-static method
+tv.cost1();
+}   }
