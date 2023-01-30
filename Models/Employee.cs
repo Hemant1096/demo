a44dae24b740a5bb517118ee9dcb387a841ec5a9
Hemant1096 <61898182+Hemant1096@users.noreply.github.com>
2023-01-30 21:23:55 +0530

    Add files via upload

---
+++ Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Newtonsoft.Json;
+
+namespace Test.test.Models
+{
+    [Table("employees")]
+    public class Employee
+    {
+        [Key]
+        public int employee_id { get; set; }
+        public string firstName {get;set;}
+        public string lastName {get;set;}
+        public string email { get; set; }
+        public string phone {get; set;}
+        public DateTime? hireDate {get; set;}
+        public int? job_id { get; set;}
+        public decimal salary { get; set;}
+        public int? managerid { get; set;}    
+        public int? departmentid { get; set;}
+    }
+}
---
+++ Models/adventureContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System;
+using System.Data.Entity.Infrastructure;
+using System.Configuration;
+
+namespace Test.test.Models
+{
+    public partial class adventureContext : DbContext
+    {
+        public adventureContext(DbContextOptions options) : base(options)
+        {
+        }
+        public virtual DbSet<Employee> Employee { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            if(!optionBuilder.IsConfigured)
+            {
+                optionBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["adventureconnectionstring"].ConnectionString);
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.Property(e => e.employee_id).HasColumnName("employee_id");
+                entity.Property(e => e.firstName).HasColumnName("first_name");
+                entity.Property(e => e.lastName).HasColumnName("last_name");
+                entity.Property(e => e.email).HasColumnName("email");
+                entity.Property(e => e.phone).HasColumnName("phone_number");
+                entity.Property(e => e.hireDate).HasColumnName("hire_date");
+                entity.Property(e => e.job_id).HasColumnName("job_id");
+                entity.Property(e => e.salary).HasColumnName("salary");
+                entity.Property(e => e.managerid).HasColumnName("manager_id");
+                entity.Property(e => e.departmentid).HasColumnName("department_id");
+            }            
+            );
+        }
+        public void save()
+        {
+            this.SaveChanges();
+        }
+    }
+}
